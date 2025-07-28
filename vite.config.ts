3d2a73da5e13956749e6fda08ffd23c3783241f8
Iqbal Hossain <iqbal.h.rasel@gmail.com>
2025-07-28 17:36:58 +0600

    redir3

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '' // Copies to root of dist/
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
