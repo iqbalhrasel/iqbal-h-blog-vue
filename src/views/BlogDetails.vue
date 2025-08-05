8ac08c1aa3a16bf06be69c8edbb7c34678a3bf2f
Iqbal Hossain <iqbal.h.rasel@gmail.com>
2025-08-05 21:41:46 +0600

    enhance blog details title

--- src/views/BlogDetails.vue
+++ src/views/BlogDetails.vue
@@ -33,9 +33,9 @@
       <!-- hero -->
       <div class="flex flex-col items-center md:py-8 py-7">
         <div class=" text-sm font-semibold text-gray-500 bg-gray-100 mx-auto px-4 py-1 rounded-full">{{ blog?.topic.name
-        }}
+          }}
         </div>
-        <h1 class="md:w-1/2 w-96 text-4xl font-semibold text-center mt-6 text-gray-700">{{ blog?.title }}
+        <h1 class="md:w-2/3 w-96 text-4xl font-semibold text-center mt-6 text-gray-700">{{ blog?.title }}
         </h1>
 
         <div class=" flex items-center gap-x-2 mt-5">
